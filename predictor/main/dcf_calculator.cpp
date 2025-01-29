8817d87b71b79daa796068c002975fec9889675a
spencercdz <spencerchudz@gmail.com>
2025-01-29 19:31:06 +0800

    mainly updated my cpp module. i think it is mostly done now, just need to link it back to my main.py.
    TODO:
    decide on the parameters to use for my calculate_discount_rate
    work on scrapping the data required for my module to work/

--- predictor/main/dcf_calculator.cpp
+++ predictor/main/dcf_calculator.cpp
@@ -1,18 +1,19 @@
 // main/mymodule.cpp
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
 
-// Feature to calculate yearly FCF for my main.py
+// Feature: calculate yearly FCF for my main.py
 float yearly_fcf(int operating_cf, int capital_expend) {
     return (operating_cf - capital_expend);
 }
 
 
-//Calculate Discount Rate (WACC - Weighted Avg Cost of Capital)
+// Feature: Calculate Discount Rate (WACC - Weighted Avg Cost of Capital)
 float discount_rate(/"int market_cap, int total_debt, int cost_equity, int cost_debt, int tax_rate"/) {
     //need to change the params so that it is more intuitive
 
@@ -41,60 +42,58 @@ float calculate_tax_rate(int income_tax_expense, int pre_tax_income) {
 }
 
 
-//Calculate Growth Rate of FCF, will be using Historical Growth Rate CAGR as the method
+// Feature: Calculate Growth Rate of FCF, will be using historical growth rate CAGR as the method
 float calculate_cagr(const std::vector<int>& fcf_list) {
-    // get my end year FCF
-    float latest_fcf = fcf_list.at(0);
-    // get my start year fcf
-    float earliest_fcf = fcf_list.back();
-    // get my length of list to be my years
-    int years = fcf_list.size();
-    // computer (end_fcf / start_fcf) * (1 / years) - 1 to get cagr
-    return ((latest_fcf / earliest_fcf) * (1 / years) - 1);
-}
+    float latest_fcf = static_cast<float>(fcf_list.at(0)); // get my latest FCF
+    float earliest_fcf = static_cast<float>(fcf_list.back()); // get my earliest fcf
+    int years = fcf_list.size(); // get my length of list to be my years
 
-//Estimate Future FCF using cagr
-const std::vector<int>& estimate_future_fcf(const std::vector<int>& fcf_list) {
-    /* input the list of FCFs you can scrape
-    estimate for next 5 years only */
-
-    // calculate cagr first
-    float cagr = calculate_cagr(const std::vector<int>& vec)
-
-    // now find my latest FCF, then project it for 5 years, we return the list of 5 years FCF
-    // to find 2026 FCF, take 2025fcf * (1 + growth rate in %) = 2026fcf
-
-    return future_fcf_list
-}
-
-//Calculate Terminal Value (TV)
-float calculate_tv(const std::vector<int>& future_fcf_list, float wacc, float cagr) {
-    // sum up my list into net projected FCF (for 5 yearz)
-
-    // now just apply (sum_FCF * (1 + cagr)) / (wacc - cagr) to get TV
-
+    return std::pow(latest_fcf / earliest_fcf, 1.0f / (years - 1)) - 1.0f; // compute (end_fcf / start_fcf) to power of (1 / years) - 1 to get cagr
 }
 
 
-//Calculate Present Value (PV)
-float calculate_pv(float year_fcf, float wacc, float year) {
-    return year_fcf / (1 + wacc) * (year);
+// Feature: Estimate Future FCF using cagr, estimate for next 5 years only
+std::vector<float> estimate_future_fcf(const std::vector<int>& fcf_list) {
+    float cagr = calculate_cagr(fcf_list); // calculate cagr first
+    float latest_fcf = static_cast<float>(fcf_list.back()); // now find my latest FCF, then project it for 5 years, we return the list of 5 years FCF
+    std::vector<float> future_fcf_list; // to find 2026 FCF, take 2025fcf * (1 + growth rate in %) = 2026fcf
+    future_fcf_list.reserve(5);
+
+    for (int i = 0; i < 5; i++) {
+        latest_fcf *= (1 + cagr);
+        future_fcf_list.push_back(latest_fcf);
+    } // calculate fcf for the years
+
+    return future_fcf_list;
 }
 
-//Calculate Equity Value
-float calculate_equity_value(float ) {
-    // calculate tv
 
-    // calculate pv
+// Feature: Calculate Equity Value
+float calculate_equity_value(const std::vector<float>& future_fcf_list, float wacc, float cagr, float net_debt) {
+    float tv = calculate_tv(future_fcf_list, wacc, cagr); // calculate tv
+    float discounted_tv = calculate_pv(tv, wacc, future_fcf_list.size());
+    float sum_of_discounted_pv = 0; // calculate sum of pv
 
-    // enteprise value = sum of pv of fcfs + pv of tv
+    for (size_t i = 0; i < future_fcf_list.size(); ++i) {
+        sum_of_discounted_pv += calculate_pv(future_fcf_list[i], wacc, (i + 1));
+    }
 
-    // equity value = enteprise value - net debt
+    float enterprise_value = sum_of_discounted_pv + discounted_tv; // enterprise_value = sum of pv of fcfs + pv of tv
+    return (enterprise_value - net_debt); // equity value = enterprise_value - net debt
+}
+
+float calculate_tv(const std::vector<float>& future_fcf_list, float wacc, float cagr) { //Calculate Terminal Value (TV)
+    float last_future_fcf = future_fcf_list.back(); // get last projected FCF (for 5 yearz)
+
+    return (last_future_fcf * (1 + cagr)) / (wacc - cagr); // now just apply (last_fcf * (1 + cagr)) / (wacc - cagr) to get TV
+}
+
+float calculate_pv(float year_fcf, float wacc, float year) { //Calculate Present Value (PV)
+    return year_fcf / (std::pow((1 + wacc), (year)));
 }
 
 
-//Calculate Intrinsic Value Per Share
+// Feature: Calculate Intrinsic Value Per Share
 float calculate_intrinsic_value(float equity_value, int shares_outstanding) {
-    // instrinc value = equity_value / shares_outstanding
-    return equity_value / shares_outstanding;
+    return equity_value / shares_outstanding; // instrinc value = equity_value / shares_outstanding
 }
--- predictor/main/main.py
+++ predictor/main/main.py
@@ -9,39 +9,43 @@ import matplotlib.pyplot as plt
 app = Flask(__name__)
 app.secret_key = 'owadio'
 
-def scrape_time():
-    pass
-
 def scrape_stock_details(ticker: str):
     stock_data = yf.Ticker(ticker)
     stock_info = stock_data.info
     filtered_stock_info = filter_stock_info(stock_info)
-    stock_cash_flow = stock_data.get_cashflow(proxy=None, as_dict=True, pretty=False, freq='yearly')
+    #stock_cash_flow = stock_data.get_cashflow(proxy=None, as_dict=True, pretty=False, freq='yearly')
 
 
     #for key,value in filtered_stock_info.items():
         #print(f"{key}: {value}")
-    print(stock_cash_flow)
+    #print(stock_cash_flow)
     pass
 
 def filter_stock_info(stock: dict):
-    filtered_info = {}
-    filtered_info['Name'] = stock['longName']
-    filtered_info['Symbol'] = stock['symbol']
-    filtered_info['Current Price'] = stock['currentPrice']
-    filtered_info['Currency'] = stock['currency']
-    filtered_info['Market Cap'] = stock['marketCap']
-    filtered_info['Volume'] = stock['volume']
-    filtered_info['Day High'] = stock['dayHigh']
-    filtered_info['Day Low'] = stock['dayLow']
-    filtered_info['Trailing PE'] = stock['trailingPE']
-    filtered_info['Trailing EPS'] = stock['trailingEps']
-    filtered_info['52 Week High'] = stock['fiftyTwoWeekHigh']
-    filtered_info['52 Week Low'] = stock['fiftyTwoWeekLow']
-    filtered_info['Summary'] = stock['longBusinessSummary']
-    return filtered_info
+    keys_map = {
+        'longName': 'Name',
+        'symbol': 'Symbol',
+        'currentPrice': 'Current Price',
+        'currency': 'Currency',
+        'marketCap': 'Market Cap',
+        'volume': 'Volume',
+        'dayHigh': 'Day High',
+        'dayLow': 'Day Low',
+        'trailingPE': 'Trailing PE',
+        'trailingEps': 'Trailing EPS',
+        'fiftyTwoWeekHigh': '52 Week High',
+        'fiftyTwoWeekLow': '52 Week Low',
+        'longBusinessSummary': 'Summary'
+    }
+
+    return {value: stock[key] for key, value in keys_map.items() if key in stock}
 
 def dcf_valuation():
+    # total_debt, cost_of_equity, cost_of_debt, tax_rate = 
+    #
+    #
+    #
+    #
     pass
 
 def scrape_stock_price(ticker: str):
