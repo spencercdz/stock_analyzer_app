3b0ba528be8489587e9e46bfd4d4ecbf2cbda8e4
spencercdz <spencerchudz@gmail.com>
2025-02-01 00:56:41 +0800

    updated main.py TODO: need to change features of cpp module (add calculate g) create my determine growth rate function (cause rn its preventing me from calculating intrinsic value as intended)

--- predictor/main/dcf_calculator.cpp
+++ predictor/main/dcf_calculator.cpp
@@ -8,48 +8,48 @@
 namespace py = pybind11;
 
 // Feature: calculate yearly FCF for my main.py
-float yearly_fcf(float operating_cf, float capital_expend) {
+double yearly_fcf(double operating_cf, double capital_expend) {
     return (operating_cf - capital_expend);
 }
 
 // Feature: Calculate Discount Rate (WACC - Weighted Avg Cost of Capital)
-float discount_rate(float market_cap, float total_debt, float cost_equity, float cost_debt, float tax_rate) {
+double discount_rate(double market_cap, double total_debt, double cost_equity, double cost_debt, double tax_rate) {
 
-    return (static_cast<float>(market_cap) / (market_cap + total_debt) * cost_equity) + (static_cast<float>(total_debt) / (market_cap + total_debt) * cost_debt * (1 - tax_rate));
+    return (static_cast<double>(market_cap) / (market_cap + total_debt) * cost_equity) + (static_cast<double>(total_debt) / (market_cap + total_debt) * cost_debt * (1 - tax_rate));
 }
 
-float calculate_total_debt(float short_term_debt, float long_term_debt) {
+double calculate_total_debt(double short_term_debt, double long_term_debt) {
     return short_term_debt + long_term_debt;
 }
 
-float calculate_cost_of_equity(float risk_free_rate, float stock_beta, float market_risk_premium) {
-    return risk_free_rate + stock_beta * (market_risk_premium);
+double calculate_cost_of_equity(double risk_free_rate, double stock_beta, double market_return) {
+    return risk_free_rate + stock_beta * (market_return - risk_free_rate);
 }
 
-float calculate_cost_of_debt(float interest_expense, float total_debt) {
-    return static_cast<float>(interest_expense) / total_debt;
+double calculate_cost_of_debt(double interest_expense, double total_debt) {
+    return static_cast<double>(interest_expense) / total_debt;
 }
 
-float calculate_tax_rate(float income_tax_expense, float pre_tax_income) {
-    return static_cast<float>(income_tax_expense) / pre_tax_income;
+double calculate_tax_rate(double income_tax_expense, double pre_tax_income) {
+    return static_cast<double>(income_tax_expense) / pre_tax_income;
 }
 
 
 // Feature: Calculate Growth Rate of FCF, will be using historical growth rate CAGR as the method
-float calculate_cagr(const std::vector<int>& fcf_list) {
-    float latest_fcf = static_cast<float>(fcf_list.at(0)); // get my latest FCF
-    float earliest_fcf = static_cast<float>(fcf_list.back()); // get my earliest fcf
+double calculate_cagr(const std::vector<double>& fcf_list) {
+    double latest_fcf = static_cast<double>(fcf_list.at(0)); // get my latest FCF
+    double earliest_fcf = static_cast<double>(fcf_list.back()); // get my earliest fcf
     size_t num_years = fcf_list.size(); // get my length of list to be my years
-    float exponent = 1.0f / (num_years - 1);
+    double exponent = 1.0f / (num_years - 1);
     return std::pow(latest_fcf / earliest_fcf, exponent) - 1.0f; // compute (end_fcf / start_fcf) to power of (1 / years) - 1 to get cagr
 }
 
 
 // Feature: Estimate Future FCF using cagr, estimate for next 5 years only
-std::vector<float> estimate_future_fcf(const std::vector<int>& fcf_list) {
-    float cagr = calculate_cagr(fcf_list); // calculate cagr first
-    float latest_fcf = static_cast<float>(fcf_list.back()); // now find my latest FCF, then project it for 5 years, we return the list of 5 years FCF
-    std::vector<float> future_fcf_list; // to find 2026 FCF, take 2025fcf * (1 + growth rate in %) = 2026fcf
+std::vector<double> estimate_future_fcf(const std::vector<double>& fcf_list) {
+    double cagr = calculate_cagr(fcf_list); // calculate cagr first
+    double latest_fcf = static_cast<double>(fcf_list.back()); // now find my latest FCF, then project it for 5 years, we return the list of 5 years FCF
+    std::vector<double> future_fcf_list; // to find 2026 FCF, take 2025fcf * (1 + growth rate in %) = 2026fcf
     future_fcf_list.reserve(5);
 
     for (int i = 0; i < 5; i++) {
@@ -60,46 +60,46 @@ std::vector<float> estimate_future_fcf(const std::vector<int>& fcf_list) {
     return future_fcf_list;
 }
 
-float calculate_tv(const std::vector<float>& future_fcf_list, float wacc, float cagr) { //Calculate Terminal Value (TV)
-    float last_future_fcf = future_fcf_list.back(); // get last projected FCF (for 5 yearz)
+double calculate_tv(const std::vector<double>& future_fcf_list, double wacc, double cagr) { //Calculate Terminal Value (TV)
+    double last_future_fcf = future_fcf_list.back(); // get last projected FCF (for 5 yearz)
 
     return (last_future_fcf * (1 + cagr)) / (wacc - cagr); // now just apply (last_fcf * (1 + cagr)) / (wacc - cagr) to get TV
 }
 
-float calculate_pv(float year_fcf, float wacc, float year) { //Calculate Present Value (PV)
+double calculate_pv(double year_fcf, double wacc, double year) { //Calculate Present Value (PV)
     return year_fcf / (std::pow((1 + wacc), (year)));
 }
 
 // Feature: Calculate Equity Value
-float calculate_equity_value(const std::vector<float>& future_fcf_list, float wacc, float cagr, float net_debt) {
-    float tv = calculate_tv(future_fcf_list, wacc, cagr); // calculate tv
-    float discounted_tv = calculate_pv(tv, wacc, static_cast<float>(future_fcf_list.size()));
-    float sum_of_discounted_pv = 0; // calculate sum of pv
+double calculate_equity_value(const std::vector<double>& future_fcf_list, double wacc, double cagr, double net_debt) {
+    double tv = calculate_tv(future_fcf_list, wacc, cagr); // calculate tv
+    double discounted_tv = calculate_pv(tv, wacc, static_cast<double>(future_fcf_list.size()));
+    double sum_of_discounted_pv = 0; // calculate sum of pv
 
     for (size_t i = 0; i < future_fcf_list.size(); ++i) {
-        sum_of_discounted_pv += calculate_pv(future_fcf_list[i], wacc, static_cast<float>(i + 1));
+        sum_of_discounted_pv += calculate_pv(future_fcf_list[i], wacc, static_cast<double>(i + 1));
     }
 
-    float enterprise_value = sum_of_discounted_pv + discounted_tv; // enterprise_value = sum of pv of fcfs + pv of tv
+    double enterprise_value = sum_of_discounted_pv + discounted_tv; // enterprise_value = sum of pv of fcfs + pv of tv
     return (enterprise_value - net_debt); // equity value = enterprise_value - net debt
 }
 
 // Feature: Calculate Intrinsic Value Per Share
-float calculate_intrinsic_value(float equity_value, int shares_outstanding) {
+double calculate_intrinsic_value(double equity_value, double shares_outstanding) {
     return equity_value / shares_outstanding; // instrinc value = equity_value / shares_outstanding
 }
 
 PYBIND11_MODULE(dcf_calculator, m) {
-    m.def("yearly_fcf", &yearly_fcf, "Feature: calculate yearly FCF for my main.py. Usage: float yearly_fcf(int operating_cf, int capital_expend)");
-    m.def("discount_rate", &discount_rate, "Feature: Calculate Discount Rate/WACC: Usage: float discount_rate(float market_cap, float total_debt, float cost_equity, float cost_debt, float tax_rate)");
-    m.def("calculate_total_debt", &calculate_total_debt, "Usage: float calculate_total_debt(float short_term_debt, float long_term_debt))");
-    m.def("calculate_cost_of_equity", &calculate_cost_of_equity, "Usage: float calculate_cost_of_equity(float risk_free_rate, float stock_beta, float market_risk_premium)");
-    m.def("calculate_cost_of_debt", &calculate_cost_of_debt, "Usage: float calculate_cost_of_debt(float interest_expense, float total_debt)");
-    m.def("calculate_tax_rate", &calculate_tax_rate, "Usage: float calculate_tax_rate(float income_tax_expense, float pre_tax_income)");
-    m.def("calculate_cagr", &calculate_cagr, "Feature: Calculate Growth Rate of FCF, will be using historical growth rate CAGR as the method. Usage: float calculate_cagr(const std::vector<int>& fcf_list)");
-    m.def("estimate_future_fcf", &estimate_future_fcf, "Feature: Estimate Future FCF using cagr, estimate for next 5 years only. Usage: std::vector<float> estimate_future_fcf(const std::vector<int>& fcf_list)");
-    m.def("calculate_equity_value", &calculate_equity_value, "Feature: Calculate Equity Value. Usage: float calculate_equity_value(const std::vector<float>& future_fcf_list, float wacc, float cagr, float net_debt)");
-    m.def("calculate_tv", &calculate_tv, "Usage: float calculate_tv(const std::vector<float>& future_fcf_list, float wacc, float cagr)");
-    m.def("calculate_pv", &calculate_pv, "Usage: float calculate_pv(float year_fcf, float wacc, float year)");
-    m.def("calculate_intrinsic_value", &calculate_intrinsic_value, "// Feature: Calculate Intrinsic Value Per Share. Usage: float calculate_intrinsic_value(float equity_value, int shares_outstanding)");
+    m.def("yearly_fcf", &yearly_fcf, "Feature: calculate yearly FCF for my main.py. Usage: double yearly_fcf(double operating_cf, double capital_expend)");
+    m.def("discount_rate", &discount_rate, "Feature: Calculate Discount Rate/WACC: Usage: double discount_rate(double market_cap, double total_debt, double cost_equity, double cost_debt, double tax_rate)");
+    m.def("calculate_total_debt", &calculate_total_debt, "Usage: double calculate_total_debt(double short_term_debt, double long_term_debt))");
+    m.def("calculate_cost_of_equity", &calculate_cost_of_equity, "Usage: double calculate_cost_of_equity(double risk_free_rate, double stock_beta, double market_risk_premium)");
+    m.def("calculate_cost_of_debt", &calculate_cost_of_debt, "Usage: double calculate_cost_of_debt(double interest_expense, double total_debt)");
+    m.def("calculate_tax_rate", &calculate_tax_rate, "Usage: double calculate_tax_rate(double income_tax_expense, double pre_tax_income)");
+    m.def("calculate_cagr", &calculate_cagr, "Feature: Calculate Growth Rate of FCF, will be using historical growth rate CAGR as the method. Usage: double calculate_cagr(const std::vector<int>& fcf_list)");
+    m.def("estimate_future_fcf", &estimate_future_fcf, "Feature: Estimate Future FCF using cagr, estimate for next 5 years only. Usage: std::vector<double> estimate_future_fcf(const std::vector<int>& fcf_list)");
+    m.def("calculate_equity_value", &calculate_equity_value, "Feature: Calculate Equity Value. Usage: double calculate_equity_value(const std::vector<double>& future_fcf_list, double wacc, double cagr, double net_debt)");
+    m.def("calculate_tv", &calculate_tv, "Usage: double calculate_tv(const std::vector<double>& future_fcf_list, double wacc, double cagr)");
+    m.def("calculate_pv", &calculate_pv, "Usage: double calculate_pv(double year_fcf, double wacc, double year)");
+    m.def("calculate_intrinsic_value", &calculate_intrinsic_value, "// Feature: Calculate Intrinsic Value Per Share. Usage: double calculate_intrinsic_value(double equity_value, double shares_outstanding)");
 }
--- predictor/main/main.py
+++ predictor/main/main.py
@@ -5,6 +5,7 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import dcf_calculator as dcf
+import numpy as np
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app.secret_key = 'owadio'
@@ -14,18 +15,18 @@ def home():
     if request.method == 'POST':
         stock_ticker = request.form.get('ticker')
         stock_ticker = stock_ticker.upper()
-        stock_info, stock_financials = scrape_stock_details(stock_ticker)
+        stock_info, stock_financials, fcf_dict = scrape_stock_details(stock_ticker)
     return render_template('home.html')
 
 def scrape_stock_details(ticker: str):
     stock_ticker = yf.Ticker(ticker)
 
     filtered_stock_info = filter_stock_summary(stock_ticker.info)
-    filtered_stock_financials, fcf = filter_stock_financials(stock_ticker)
-    print(filtered_stock_financials)
-    print(fcf)
+    filtered_stock_financials, fcf = filter_stock_financials(stock_ticker, filtered_stock_info)
+    intrinsic_value = calculate_intrinsic_value_dcf(filtered_stock_financials, fcf)
+    
 
-    return filtered_stock_info, filtered_stock_financials
+    return filtered_stock_info, filtered_stock_financials, fcf
 
 def filter_stock_summary(stock: dict):
     keys_map = {
@@ -41,54 +42,85 @@ def filter_stock_summary(stock: dict):
         'trailingEps': 'Trailing EPS',
         'fiftyTwoWeekHigh': '52 Week High',
         'fiftyTwoWeekLow': '52 Week Low',
-        'longBusinessSummary': 'Summary'
+        'longBusinessSummary': 'Summary',
+        'beta': 'Beta'
     }
     return {value: stock[key] for key, value in keys_map.items() if key in stock}
 
-def filter_stock_financials(ticker: str):
+def filter_stock_financials(ticker: str, filtered_stock_info: dict):
     keys_income_statement = {
         'InterestExpense': 'interest_expense',
         'TaxProvision' : 'tax_provision',
         'PretaxIncome': 'pretax_income',
-        'DilutedAverageShares': 'diluted_average_shares'
+        'DilutedAverageShares': 'diluted_average_shares',
+        'EBIT': 'ebit'
     }
 
     keys_balance_sheet = {
         'TotalDebt': 'total_debt',
         'CashAndCashEquivalents': 'cash_and_cash_equivalents',
+        'InvestedCapital': 'invested_capital'
     }
 
+    keys_cash_flow = {
+        'CapitalExpenditure': 'capex',
+        'ChangeInWorkingCapital': 'change_in_working_capital'
+    }
+    # add in income statement variables
     income_statement = ticker.get_income_stmt(as_dict=True, freq='yearly')
     income_statement_key = next(iter(income_statement))
-
+    results = {value: income_statement[income_statement_key][key] for key, value in keys_income_statement.items() if key in income_statement[income_statement_key]}
+    # add in balance_sheet variables
     balance_sheet = ticker.get_balance_sheet(as_dict=True, freq='yearly')
     balance_sheet_key = next(iter(balance_sheet))
-
-    cash_flow = ticker.get_cashflow(as_dict=True, freq='yearly')
-    
-    results = {value: income_statement[income_statement_key][key] for key, value in keys_income_statement.items() if key in income_statement[income_statement_key]}
     results.update({value: balance_sheet[balance_sheet_key][key] for key, value in keys_balance_sheet.items() if key in balance_sheet[balance_sheet_key]})
-    
+    # add in cash_flow variables
+    cash_flow = ticker.get_cashflow(as_dict=True, freq='yearly')
+    cash_flow_key = next(iter(cash_flow))
+    results.update({value: cash_flow[cash_flow_key][key] for key, value in keys_cash_flow.items() if key in cash_flow[cash_flow_key]})
+    # add in stock info
+    results['market_cap'] = filtered_stock_info['Market Cap']
+    results['beta'] = filtered_stock_info['Beta']
+    # perform and add some calculations
+    results['cost_of_equity'] = dcf.calculate_cost_of_equity((4.0/100), results['beta'], (9.5/100)) # rfr = 4, mr = 9.5. for now we fix the Treasury Rate and Market Return, have to make a separate scraper to get these values for the specific country the company is in
+    results['cost_of_debt'] = dcf.calculate_cost_of_debt(results['interest_expense'], results['total_debt'])
+    results['tax_rate'] = dcf.calculate_tax_rate(results['tax_provision'], results['pretax_income'])
+    results['net_debt'] = results['total_debt'] - results['cash_and_cash_equivalents']
+
+    # calculate fcf from cash flow list
     fcf = {}
     for timestamp, data in cash_flow.items():
-        year = timestamp.year
-        fcf[year] = data['FreeCashFlow']
-
+        free_cash_flow = data.get('FreeCashFlow', None)
+        if free_cash_flow and not pd.isna(free_cash_flow):
+            year = timestamp.year
+            fcf[year] = data['FreeCashFlow']
     return results, fcf
 
-def generate_fcf_over_years(ticker):
-    pass
+def calculate_intrinsic_value_dcf(data_source: dict, fcf_list):#
+    market_cap, total_debt, cost_of_equity, cost_of_debt, tax_rate, net_debt, shares_outstanding = data_source['market_cap'], data_source['total_debt'], data_source['cost_of_equity'], data_source['cost_of_debt'], data_source['tax_rate'], data_source['net_debt'], data_source['diluted_average_shares']
+    
+    # CALCUALTE GROWTH RATE G
+    ebit, invested_capital, capex, change_in_working_capital = data_source['ebit'], data_source['invested_capital'], data_source['capex'], data_source['change_in_working_capital']
+    g = ((ebit * (1 - tax_rate)) / invested_capital) * ((capex + change_in_working_capital) / (ebit * (1 - tax_rate)))
+    print(f"g = {g}")
 
-def calculate_intrinsic_value_dcf(data_source: dict):
-    market_cap, total_debt, cost_equity, cost_debt, tax_rate, net_debt, shares_outstanding = data_source['market_cap'], data_source['total_debt'], data_source['cost_equity'], data_source['cost_debt'], data_source['tax_rate'], data_source['net_debt'], data_source['shares_outstanding']
-    fcf_list = generate_fcf_over_years(data_source['ticker'])
+    # calculate industry growth rate
+    industry_growth_rate = 0.05 # fix it for now, later we can scrape it out no worries
 
-    wacc = dcf.discount_rate(market_cap, total_debt, cost_equity, cost_debt, tax_rate)
-    cagr = dcf.calculate_cagr(fcf_list)
+    # Calculate growth rates from FCF list
+    fcf_list = list(fcf_list.values())
     future_fcf_list = dcf.estimate_future_fcf(fcf_list)
-    equity_value = dcf.calculate_equity_value(future_fcf_list, wacc, cagr, net_debt)
+    wacc = dcf.discount_rate(market_cap, total_debt, cost_of_equity, cost_of_debt, tax_rate)
+    cagr = dcf.calculate_cagr(fcf_list)
+    chosen_growth_rate = estimate_growth_rate(fcf_list, cagr, g, industry_growth_rate, wacc)
+    equity_value = dcf.calculate_equity_value(future_fcf_list, wacc, chosen_growth_rate, net_debt)
     intrinsic_value = dcf.calculate_intrinsic_value(equity_value, shares_outstanding)
+    print(intrinsic_value)
     return intrinsic_value
+
+def estimate_growth_rate(fcf_list, cagr, g, industry_growth_rate, wacc):
+    return wacc - 0.02
+
 
 def scrape_stock_price(ticker: str):
     # stock_data.history(period="5y")
